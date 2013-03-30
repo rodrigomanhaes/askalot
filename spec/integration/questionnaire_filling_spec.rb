78cf098782fe9f017d2af6a81861de495cbfa18e
Rodrigo Manhães <rmanhaes@gmail.com>
2013-03-30 03:22:32 -0300

    choose multiple options

--- spec/integration/questionnaire_filling_spec.rb
+++ spec/integration/questionnaire_filling_spec.rb
@@ -1,20 +1,35 @@
 require 'spec_helper'
 
 feature 'questionnaire filling' do
-  scenario 'basic multiple choice' do
+  let(:questionnaire) do
     questionnaire = create(:questionnaire)
     create_question(questionnaire, 'Who discovered Brazil?',
       'Pedro Alvares Cabral', 'Diego Armando Maradona', 'Cristoforo Colombo',
       'Marco Polo')
     create_question(questionnaire, "Who invented the airplane?",
       "Wachowski Brothers", "Mario Brothers", "Alberto Santos-Dumont",
-      "Warner Brothers")
+      "Warner Brothers", multiple: true)
+    questionnaire
+  end
 
+  before(:each) do
     visit new_questionnaire_answer_path(questionnaire)
+  end
+
+  scenario 'basic multiple choice' do
     check "Pedro Alvares Cabral"
     check "Alberto Santos-Dumont"
     click_button "Answer"
     page.should have_content 'Who discovered Brazil? Pedro Alvares Cabral'
     page.should have_content 'Who invented the airplane? Alberto Santos-Dumont'
+  end
+
+  scenario 'choose multiple options' do
+    check "Pedro Alvares Cabral"
+    check "Alberto Santos-Dumont"
+    check "Mario Brothers"
+    click_button "Answer"
+    page.should have_content 'Who discovered Brazil? Pedro Alvares Cabral'
+    page.should have_content 'Who invented the airplane? Mario Brothers, Alberto Santos-Dumont'
   end
 end
