9b1cd2df6be7eb8d39379951e094bbb4521146b7
Rodrigo Manhães <rmanhaes@gmail.com>
2013-03-31 05:04:42 -0300

    remove unused stub method

--- spec/helpers/askalot/questionnaires_helper_spec.rb
+++ spec/helpers/askalot/questionnaires_helper_spec.rb
@@ -4,8 +4,7 @@ module Askalot
   describe QuestionnairesHelper do
     it 'generates id for option fields using questionnaire, question and option ids' do
       questionnaire = stub_model(Questionnaire, id: 11)
-      question = stub_model(Question, id: 22, questionnaire: questionnaire,
-        multiple: false)
+      question = stub_model(Question, id: 22, questionnaire: questionnaire)
       option = stub_model(Option, id: 33, question: question)
       helper.option_id(option).should == '11_22_33'
     end
